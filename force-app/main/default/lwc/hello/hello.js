46bd16caa5356bcf10145e9e485a268617d1f70f
nguyevminh <>
2020-12-28 13:46:59 +0000

    [ClickDeploy] Default to GitHub DX

--- force-app/main/default/lwc/hello/hello.js
+++ force-app/main/default/lwc/hello/hello.js
@@ -1,8 +1,10 @@
 import { LightningElement } from 'lwc';
 import My_Custom_Label from '@salesforce/label/c.My_Custom_Label';
+import Blackburn from '@salesforce/label/c.sb_Blackburn';
 
 export default class Hello extends LightningElement {
     label = {
-        My_Custom_Label
+        My_Custom_Label,
+        Blackburn
     };
 }
